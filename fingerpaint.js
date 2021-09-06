1d115018c36b87a98506857c466e6e3074bd644e
Ankit <ankitrastogi31200@gmail.com>
2021-09-06 15:28:03 +0530

    initial

---
+++ fingerpaint.js
@@ -0,0 +1,113 @@
+var drawing = false;
+var context;
+var brushColor='000';
+var restore=[];
+var currentIndex=-1;
+
+window.onload=function()
+{
+
+    //Clear Button
+    document.getElementById('btnClear').addEventListener('click', function(){
+            context.clearRect(0,0, context.canvas.width, context.canvas.height);    
+            restore=[];
+            currentIndex=-1;   
+    }, false);
+    
+    //Width Scale
+    document.getElementById('lineWidth').addEventListener('change', function(){
+            context.lineWidth = document.getElementById('lineWidth').value;
+    }, false);
+    
+    //Color
+    document.getElementById('colorChange').addEventListener('change', function(){
+            context.strokeStyle = document.getElementById('colorChange').value;
+            brushColor=document.getElementById('colorChange').value;;
+    }, false);
+
+
+    //Eraser Button
+    document.getElementById('eraser').addEventListener('click', function(){
+        context.strokeStyle = "#fff"; 
+        // restore.push(context.getImageData(0,0,context.canvas.width, context.canvas.height))   
+    }, false);
+
+    //Brush Button
+    document.getElementById('brush').addEventListener('click', function(){
+        context.strokeStyle = document.getElementById('colorChange').value;
+        // restore.push(context.getImageData(0,0,context.canvas.width, context.canvas.height))
+    }, false);
+
+    //Undo Button
+    document.getElementById('undo').addEventListener('click', function(){
+        if(currentIndex<=0){
+            context.clearRect(0,0, context.canvas.width, context.canvas.height);    
+            currentIndex=-1;     
+        }else{
+            currentIndex--;
+            context.putImageData(restore[currentIndex],0,0);
+        }
+    }, false);
+
+    //Redo Button
+    document.getElementById('redo').addEventListener('click', function(){
+        console.log(currentIndex,restore.length);
+        if(currentIndex<restore.length-1){
+            currentIndex++;
+            context.putImageData(restore[currentIndex],0,0);
+        }
+    }, false);
+
+    //Size Canvas
+    context = document.getElementById('myCanvas').getContext("2d");
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight-60;
+    
+    //Mouse movement
+    document.getElementById('myCanvas').onmousemove = handleMouseMove;
+    document.getElementById('myCanvas').onmousedown = handleDown;
+    document.getElementById('myCanvas').onmouseup = handleUp;
+    
+    //Style line
+    context.strokeStyle = "#000";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+    
+    //Hide Save Area
+    document.getElementById('saveArea').style.display = "none";
+}
+
+function handleMouseMove(e)
+{
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+    if(drawing)
+    {
+       
+        context.lineTo(e.clientX, e.clientY);
+        context.closePath();
+        context.stroke();
+        context.moveTo(e.clientX, e.clientY);
+    } else
+    {
+        context.moveTo(e.clientX, e.clientY);
+    }
+    
+}
+
+function handleDown(e)
+{
+    drawing = !drawing; 
+    console.log('handleDown',drawing);
+    context.moveTo(e.clientX, e.clientY);
+    context.beginPath();
+     
+}
+
+function handleUp()
+{
+    drawing = !drawing;
+    restore.push(context.getImageData(0,0,context.canvas.width, context.canvas.height))
+    currentIndex++;
+    console.log(restore);
+}
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadString, getDownloadURL, listAll  } from "firebase/storage";
+
+var photo = false;
+const firebaseConfig = {
+  apiKey: "AIzaSyB-B2cb-jZAK9WAF6M1dXdergLoIGoAJQg",
+  authDomain: "ourdoodler.firebaseapp.com",
+  projectId: "ourdoodler",
+  storageBucket: "ourdoodler.appspot.com",
+  messagingSenderId: "789115214344",
+  appId: "1:789115214344:web:2f3d8b19af4b9ba9a8cfbd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const storage = getStorage();
+const storageRef = ref(storage, 'Images/'+new Date());
+
+document.getElementById('btnSave').addEventListener('click', function(){
+  uploadString(storageRef, document.getElementById('myCanvas').toDataURL(), 'data_url').then((snapshot) => {
+    alert('Image Uploaded!!')
+  });
+}, false);
+
+document.getElementById('btnShow').addEventListener('click', function(){
+  if(photo){
+    photo=false;
+    document.getElementById('btnShow').innerHTML='Show Photos'
+    document.querySelector("#myCanvas").style.display='block'
+    document.querySelector(".myPhotos").innerHTML=''
+    document.querySelector(".myPhotos").style.display='none'
+    document.querySelector(".tools").style.display='flex'
+  }else{
+    photo=true;
+    document.getElementById('btnShow').innerHTML='Show Canvas'
+    document.querySelector("#myCanvas").style.display='none'
+    document.querySelector(".myPhotos").style.display='block'
+    document.querySelector(".tools").style.display='none'
+    show();
+  }
+}, false);
+
+const show = () => {
+  var head = document.createElement('h1');
+  head.innerHTML = 'My Photos';
+  document.querySelector(".myPhotos").appendChild(head);
+  const storageRef = ref(storage, 'Images/');
+  listAll(storageRef)
+    .then((res)=>res.items.forEach((itemRef) => {
+      getDownloadURL(itemRef).then((url)=>{
+        console.log(url);
+        var img = document.createElement('img');
+            img.src = url;
+            img.width = '350';
+            document.querySelector(".myPhotos").appendChild(img);
+      })
+
+    }));
+}
